5818f5ccb178e96fb0408cbb4f297f4d6f90cb80
Peter VandeHaar <pjvandehaar@gmail.com>
2015-09-02 16:38:24 -0400

    soupify

---
+++ soupify.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import bs4
+import sys
+
+mess_of_input = sys.stdin.read()
+
+if len(sys.argv) > 1 and sys.argv[1] == '--xml':
+    soup = bs4.BeautifulSoup(mess_of_input, features='xml')
+else:
+    soup = bs4.BeautifulSoup(mess_of_input, 'html.parser')
+
+print(soup.prettify())
