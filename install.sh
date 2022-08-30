17baca3fffb31275e65161a56f2f51407c7b0719
Peter VandeHaar <pjvandehaar@gmail.com>
2022-08-30 12:53:08 -0400

    .

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-readlinkf() { perl -MCwd -le 'print Cwd::abs_path shift' "$1"; }
-dotfiles_path="$(cd "$(dirname "$(readlinkf "${BASH_SOURCE[0]}")")" && echo "$PWD")"
-python3 "$dotfiles_path/install.py"
